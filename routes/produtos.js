96d967ff2ea3b0d3fecb819bc4ab074eafabad00
Benjamim-EP <bannk18@gmail.com>
2022-11-02 19:43:29 -0300

    Melhor organização da Api

--- routes/produtos.js
+++ routes/produtos.js
@@ -67,13 +67,30 @@ router.get('/:id_produto',(req,res,next) =>{
         conn.query(
             'SELECT * FROM produtos WHERE id_produtos = ?',
             [req.params.id_produto],
-            (error,resultado,fields) => {
+            (error,result,fields) => {
                 if(error) { return res.status(500).send({ error:error})}
-                return res.status(200).send({response: resultado})
+                
+                if (result.lenght == 0){
+                    return res.status(404).send({
+                        mensagem: 'Nâo foi encontrado cin este ID'
+                    })
+                }
+                const response = {
+                    produto: {
+                        id_produto: result[0].id_produto,
+                        nome: result[0].nome,
+                        preco: {
+                            tipo: 'GET',
+                            descricao: 'Retorna um produto',
+                            url: 'http://localhost:3000/produtos'
+                        }
+                    }
+                }
+                return res.status(201).send(response);
             }
         )
     })
-
+})
 router.patch('/',(req, res, next) => {
     mysql.getConnection((error, conn) => {
         conn.query(
@@ -82,16 +99,28 @@ router.patch('/',(req, res, next) => {
                     preco        = ?
                 WHERE id_produtos = ?`,
             [
-                re.body.nome,
+                req.body.nome,
                 req.body.preco,
-                re.body.id_produtos
+                req.body.id_produtos
             ],
             (error, resultado, field) => {
                 conn.release(); // liberar conexão
                 if(error){ return res.status(500).send({error: error,responde: null})}
-                res.status(201).send({
-                    mensagem: 'produto alterado'
-                })
+                const response ={
+                    mensagem: 'Produto atualizado com sucesso',
+                    produtoCriado: {
+                        id_produto: resultado.id_produto,
+                        nome: req.body.nome,
+                        preco: req.body.preco,
+                        request: {
+                            tipo: 'GET',
+                            descricao: 'Detalhes de um produto',
+                            url: 'http://localhost:3000/produtos/' + req.body.id_produto
+                        }
+                    }
+                }
+                
+                return res.status(202).send(response)
             }
         )
     })
@@ -100,21 +129,29 @@ router.patch('/',(req, res, next) => {
 router.delete('/',(req, res, next) => {
     mysql.getConnection((error, conn) => {
         conn.query(
-            `DELETE FROM produtos WHERE id_produto = ?`,
+            `DELETE FROM produtos WHERE id_produtos = ?`,
             [
-                re.body.id_produtos
+                req.body.id_produtos
             ],
             (error, resultado, field) => {
                 conn.release(); // liberar conexão
                 if(error){ return res.status(500).send({error: error,responde: null})}
-                res.status(202).send({
-                    mensagem: 'produto removido'
-                })
+                const response = {
+                    mensagem: 'Produto removido com sucesso',
+                    request:{
+                        tipo: 'POST',
+                        descricao: 'remove um produto',
+                        url: 'http://localhost:3000/produtos',
+                        body:{
+                            nome: 'String',
+                            preco: 'Number'
+                        }
+                    }
+                }
+                return res.status(202).send(response);
             }
         )
     })
-})
-
 })
 
 
